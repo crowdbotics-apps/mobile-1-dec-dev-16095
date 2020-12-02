3a6c72d1957037e36374c6519f7772ebc63ccf03
Crowdbotics <team@crowdbotics.com>
2020-12-02 08:31:55 +0000

    Update components

--- src/features/BlankScreen015259/screens/blank.js
+++ src/features/BlankScreen015259/screens/blank.js
@@ -23,11 +23,16 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = { TextInput_86: "", CheckBox_104: true }
 
   render = () => (
     <View>
       <Text style={styles.Text_2}>Sample text content</Text>
+      <TextInput
+        placeholder="Sample text input placeholder"
+        value={this.state.TextInput_86}
+        onChangeText={nextValue => this.setState({ TextInput_86: nextValue })}
+      />
       <Button
         title="Press me!"
         style={styles.Button_3}
@@ -35,6 +40,15 @@ export default class Blank extends React.Component {
       />
       <Text style={styles.Text_4}>Sample text content</Text>
       <Text style={styles.Text_5}>Sample text content</Text>
+      <Icon name="star" />
+      <CheckBox
+        title="Radio button"
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
+        checked={this.state.CheckBox_104}
+        onPress={nextChecked => this.setState({ CheckBox_104: nextChecked })}
+      />
+      <Icon name="star" />
     </View>
   )
 }
@@ -254,6 +268,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
     textAlign: "center"
   },
+
   View_1: {},
   Text_2: {
     width: 150,
@@ -265,6 +280,64 @@ const styles = StyleSheet.create({
     borderTopWidth: 2,
     borderBottomWidth: 2
   },
+
+  Button_3: {
+    width: 100,
+    alignSelf: "center",
+    borderWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2
+  },
+
+  Text_4: {
+    width: 150,
+    marginTop: 10,
+    marginBottom: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    alignSelf: "center",
+    color: "#3c41d3",
+    backgroundColor: "#aaeee0",
+    borderColor: "#8049b6",
+    borderWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2,
+    textAlign: "center"
+  },
+
+  Text_5: {
+    width: 150,
+    marginTop: 20,
+    marginBottom: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    alignSelf: "center",
+    color: "#3c41d3",
+    backgroundColor: "#aaeee0",
+    borderColor: "#8049b6",
+    borderWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2,
+    textAlign: "center"
+  },
+  View_1: {},
+  Text_2: {
+    width: 150,
+    alignSelf: "center",
+    borderColor: "#8049b6",
+    borderWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2
+  },
+  TextInput_86: {},
   Button_3: {
     width: 100,
     alignSelf: "center",
@@ -307,5 +380,8 @@ const styles = StyleSheet.create({
     borderTopWidth: 2,
     borderBottomWidth: 2,
     textAlign: "center"
-  }
+  },
+  Icon_83: {},
+  CheckBox_104: {},
+  Icon_216: {}
 })
